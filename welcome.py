3447431dc77c811fadf501825456353d052f710f
Zoe <dadac123@gmail.com>
2015-10-28 22:37:26 +0800

    try to add gif to the frame

--- welcome.py
+++ welcome.py
@@ -84,6 +84,13 @@ class StartPage(tk.Frame):
         button1.pack()
         button2.pack()
 
+        self.photo1=Tkinter.PhotoImage(file="images/write_200.gif")
+        canvas=Tkinter.Canvas(frame, width=self.photo1.width(), height=self.photo1.height())
+        canvas.create_image(0,0,anchor=NW,image=self.photo1)
+        canvas.pack()
+
+        button = Tkinter.Button(frame, text = "写日记", command = master.destroy)
+        button.pack()
 
 class PageOne(tk.Frame):
 
